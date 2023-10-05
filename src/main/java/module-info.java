bbd3617852be7c597064950c1d5747f063c7e7cc
Klaudia Bernat <klaudia.j.bernat@gmail.com>
2023-10-05 13:18:51 +0200

    Add methods to get weather from OpenWeatherMap API

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,12 @@ module Init{
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires com.google.gson;
 
-    opens pl.bernat;
-    opens pl.bernat.controller;
+    opens pl.bernat.controller to javafx.fxml, javafx.controls, com.google.gson;
+    opens pl.bernat.view to javafx.fxml, javafx.controls, com.google.gson;
+    opens pl.bernat.model to javafx.fxml, javafx.controls, com.google.gson;
+    opens pl.bernat.model.client to com.google.gson;
+    exports pl.bernat.model.client;
 }
